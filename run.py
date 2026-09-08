27bcecb1579a153b3b5d8e6d2d6765dfb9b85067
surajm45 <surajmoolya164@gmail.com>
2026-09-08 19:35:28 +0530

    Add files via upload

--- run.py
+++ run.py
@@ -5,8 +5,4 @@ from backend.app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    host = os.getenv("HOST", "0.0.0.0")
-    port = int(os.getenv("PORT", 5000))
-    debug = os.getenv("FLASK_ENV", "production") == "development"
-
-    app.run(host=host, port=port, debug=debug)
+    app.run()
